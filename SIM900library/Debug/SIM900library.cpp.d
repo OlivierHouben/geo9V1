97f9a19fabbd3b41453565561cd565ab6438de53
Olivier <olivierhouben2000@outlook.com>
2018-03-23 14:50:05 +0100

    Initial commit

---
+++ SIM900library/Debug/SIM900library.cpp.d
@@ -0,0 +1,18 @@
+C:\Users\Houbix\AppData\Local\Temp\VMBuilds\SIM900library\uno\Debug\SIM900library.cpp.o: \
+ C:\Users\Houbix\AppData\Local\Temp\VMBuilds\SIM900library\uno\Debug\SIM900library.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\GPRS_SIM900/GPRS_Shield_Arduino.h \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\GPRS_SIM900/sim900.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SoftwareSerial\src/SoftwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h
---
+++ thingeriot/Debug/thingeriot.cpp.d
@@ -0,0 +1,31 @@
+C:\Users\Houbix\AppData\Local\Temp\VMBuilds\thingeriot\uno\Debug\thingeriot.cpp.o: \
+ C:\Users\Houbix\AppData\Local\Temp\VMBuilds\thingeriot\uno\Debug\thingeriot.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\TinyGSM\src/TinyGsmClient.h \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\TinyGSM\src/TinyGsmClientSIM800.h \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\TinyGSM\src/TinyGsmCommon.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Client.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/IPAddress.h \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\TinyGSM\src/TinyGsmFifo.h \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\thinger\src/ThingerTinyGSM.h \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\thinger\src/ThingerClient.h \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\thinger\src/thinger/thinger.h \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\thinger\src/thinger/pson.h \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\thinger\src/thinger/thinger_map.hpp \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\thinger\src/thinger/thinger_resource.hpp \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\thinger\src/thinger/thinger_message.hpp \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\thinger\src/thinger/thinger_encoder.hpp \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\thinger\src/thinger/thinger_io.hpp \
+ C:\Users\Houbix\Dropbox\Arduino\libraries\thinger\src/thinger/thinger_decoder.hpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SoftwareSerial\src/SoftwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h
